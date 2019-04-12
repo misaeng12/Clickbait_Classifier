3db4520eedc8ed54059785541ea4a842871e0db9
KIM <46951541+misaeng12@users.noreply.github.com>
2019-04-12 21:47:41 +0900

    Update svm.R

--- svm.R
+++ svm.R
@@ -7,8 +7,8 @@ test.data$hoax <- as.factor(test.data$hoax)
 
 library(e1071)
 
-tune.svm <- tune.svm( hoax ~ feature1 + feature1.1 + feature2.1 + feature2.2 + feature3 + feature4 + feature5, data = train.data,
-                      gamma = seq(0.1,0.2,0.01), cost = c(0.1, 1, 10) )
+tune.svm <- tune.svm( hoax ~ feature1 + feature1.1 + feature2.1 + feature2.2 + feature3 + feature4 + feature5,
+                      data = train.data, gamma = seq(0.1,0.2,0.01), cost = c(0.1, 1, 10) )
 summary(tune.svm)
 predicted <- predict( tune.svm$best.model, newdata=test.data, decision.value=TRUE )
 
