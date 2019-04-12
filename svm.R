51ccd262bdf3ff91377a6330501d4c7b2a199491
KIM <46951541+misaeng12@users.noreply.github.com>
2019-04-12 21:48:05 +0900

    Update svm.R

--- svm.R
+++ svm.R
@@ -10,7 +10,7 @@ library(e1071)
 tune.svm <- tune.svm( hoax ~ feature1 + feature1.1 + feature2.1 + feature2.2 + feature3 + feature4 + feature5,
                       data = train.data, gamma = seq(0.1,0.2,0.01), cost = c(0.1, 1, 10) )
 summary(tune.svm)
-predicted <- predict( tune.svm$best.model, newdata=test.data, decision.value=TRUE )
+predicted <- predict( tune.svm$best.model, newdata = test.data, decision.value = TRUE )
 
 ( accuracy <- sum( ifelse(test.data$hoax == predicted, 1, 0) ) / nrow(test.data) )
 ( recall <- sum( test.data$hoax==1 & predicted==1 ) / sum( test.data$hoax==1 ) )
