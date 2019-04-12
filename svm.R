76b0a124ab9331b55ee738bd8d1c4744306516e8
KIM <46951541+misaeng12@users.noreply.github.com>
2019-04-12 21:46:34 +0900

    Add files via upload

---
+++ svm.R
@@ -0,0 +1,21 @@
+
+setwd("C:\\Users\\SONpc\\Desktop\\성이\\BOAZ 프로젝트\\modeling")
+
+train.data <- read.csv("train_featured.csv", encoding="cp949")
+test.data <- read.csv("test_featured.csv", encoding="cp949")
+
+train.data$hoax <- as.factor(train.data$hoax)
+test.data$hoax <- as.factor(test.data$hoax)
+
+
+
+library(e1071)
+
+tune.svm <- tune.svm( hoax ~ feature1 + feature1.1 + feature2.1 + feature2.2 + feature3 + feature4 + feature5, data = train.data,
+                      gamma = seq(0.1,0.2,0.01), cost = c(0.1, 1, 10) )
+summary(tune.svm)
+predicted <- predict( tune.svm$best.model, newdata=test.data, decision.value=TRUE )
+
+( accuracy <- sum( ifelse(test.data$hoax == predicted, 1, 0) ) / nrow(test.data) )
+( recall <- sum( test.data$hoax==1 & predicted==1 ) / sum( test.data$hoax==1 ) )
+( misclassification.table <- table(test.data$hoax, predicted) )
